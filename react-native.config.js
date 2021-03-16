8e64483a71ff33f6f9539d8258b6ffde90236f32
rahulekZero <66659269+rahulekZero@users.noreply.github.com>
2021-03-16 12:05:30 +0530

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -6,8 +6,8 @@ module.exports = {
             }
         },
         hooks: {
-            postlink: "node ./node_modules/@rahulekZero/react-native-background-geolocation/scripts/postlink.js",
-            postunlink: "node ./node_modules/@rahulekZero/react-native-background-geolocation/scripts/postunlink.js"
+            postlink: "node ./node_modules/@rahulekzero/react-native-background-geolocation/scripts/postlink.js",
+            postunlink: "node ./node_modules/@rahulekzero/react-native-background-geolocation/scripts/postunlink.js"
         }
     }
 };
